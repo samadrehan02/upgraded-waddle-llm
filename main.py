f5b8fee1c64d252a8dd5e484018cce6a439c31e8
samadrehan02 <samadrehan123456@gmail.com>
2026-01-19 15:35:38 +0530

    bug fixes and corrections

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-
 from app.api.websocket import ws_router
 
 app = FastAPI()
@@ -12,12 +11,10 @@ app.mount("/data", StaticFiles(directory="data"), name="data")
 
 app.include_router(ws_router)
 
-
 @app.get("/", response_class=HTMLResponse)
 def index():
     with open("templates/index.html", encoding="utf-8") as f:
         return f.read()
-
 
 if __name__ == "__main__":
     uvicorn.run(
